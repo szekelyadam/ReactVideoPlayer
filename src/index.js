e03537c9139733561abdba763f00b3f77b55e950
Ádám Székely <szekelyadam@icloud.com>
2017-03-12 18:56:31 +0100

    Adding airbnb eslint config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDT758earpxKx-a_8ziuD0LU3V0IL2vvy8';
 
 const App = () => {
-    return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
